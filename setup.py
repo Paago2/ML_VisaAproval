43b5c8f9e271bb78f3aecd2bd7058165eb07f6e7
Paago2 <paago2@yahoo.com>
2024-09-29 06:18:34 -0400

    folders/files and data updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="visa_approval",
+    version="0.0.0",
+    author="paago2",
+    author_email="paago2@yahoo.commy",
+    packages=find_packages()
+)
