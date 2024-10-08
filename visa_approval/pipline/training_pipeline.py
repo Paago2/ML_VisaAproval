e211f286c7f1daa5ba33f03bcc0d800dac2aa413
Paago2 <paago2@yahoo.com>
2024-10-08 17:47:10 -0400

    Data validation completed

--- visa_approval/pipline/training_pipeline.py
+++ visa_approval/pipline/training_pipeline.py
@@ -2,15 +2,16 @@ import sys
 from visa_approval.exception import VisaApprovalException
 from visa_approval.logger import logging
 from visa_approval.components.data_ingestion import DataIngestion
+from visa_approval.components.data_validation import DataValidation
  
-from visa_approval.entity.config_entity import DataIngestionConfig
+from visa_approval.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
 
-from visa_approval.entity.artifact_entity import DataIngestionArtifact
+from visa_approval.entity.artifact_entity import (DataIngestionArtifact, DataValidationArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -29,6 +30,30 @@ class TrainPipeline:
         except Exception as e:
             raise VisaApprovalException(e, sys) from e
  
+
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise VisaApprovalException(e, sys) from e
 
     def run_pipeline(self, ) -> None:
         """
